7782c8b1bb644472b14a769e1248228231c8f9c7
Jose Francisco Ortega <jfortegacarrasco@gmail.com>
2021-11-08 18:10:01 +0100

    Creado BaseController. Se adapta el controlador CheckCountry para extender de BaseController

--- src/Controller/Api/CheckCountry.php
+++ src/Controller/Api/CheckCountry.php
@@ -2,18 +2,17 @@
 
 namespace App\Controller\Api;
 
-use FOS\RestBundle\Controller\AbstractFOSRestController;
+use App\Controller\Api\BaseController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
  * Description of CheckCountry
  *
  * @Route("check-country", name="check_country", methods="GET")
  */
-class CheckCountry extends AbstractFOSRestController
+class CheckCountry extends BaseController
 {
 
 	private Request $request;
@@ -25,12 +24,12 @@ class CheckCountry extends AbstractFOSRestController
 
 	public function __invoke()
 	{
-		$countryCode = $this->request->request->get('country-code');
+		$countryCode = $this->request->query->get('country-code');
 		if (empty($countryCode)) {
 			throw new \Exception();
 		}
 
-		return $this->view(['code' => 0], JsonResponse::HTTP_OK);
+		return $this->getSuccessfulResponse(['code' => 0]);
 	}
 
 }
