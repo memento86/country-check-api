3381f5062ca66d1c53a697c026015b9f3409b16a
Jose Francisco Ortega <jfortegacarrasco@gmail.com>
2021-11-10 14:52:15 +0100

    Implementado servicio que evalúa el criterio que compara la población del país consultado con la de Noruega

--- src/Service/Criteria/PopulationCriteria.php
+++ src/Service/Criteria/PopulationCriteria.php
@@ -3,16 +3,22 @@
 namespace App\Service\Criteria;
 
 use App\Service\Criteria\CriteriaInterface;
+use App\Service\Http\Client\RestCountriesService;
 
 /**
- * Class that evaluates if country is in Asian region and has a population greater than 80,000 persons
+ * Class that evaluates if country is in Asia region and has a population greater than 80,000 persons
  */
 class PopulationCriteria implements CriteriaInterface
 {
-
+	
+	const REGION = 'Asia';
+	const POPULATION_MIN_ASIA = 80000000;
+	const POPULATION_MIN_NOT_ASIA = 50000000;
+	
 	public function evaluate(string $countryCode, array $restCountryResponse): bool
 	{
-		return false;
+		return ($restCountryResponse['region'] === self::REGION && $restCountryResponse['population'] > self::POPULATION_MIN_ASIA) ||
+		($restCountryResponse['region'] !== self::REGION && $restCountryResponse['population'] > self::POPULATION_MIN_NOT_ASIA);
 	}
 
 }
